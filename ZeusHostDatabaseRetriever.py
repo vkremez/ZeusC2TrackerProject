f8d673054f1ac33ec170d8b1bd09f23d62f82aee
Vitali's Repo <vkremez@users.noreply.github.com>
2016-01-11 06:25:57 -0500

    Update ZeusHostDatabaseRetriever.py

--- ZeusHostDatabaseRetriever.py
+++ ZeusHostDatabaseRetriever.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 # author = Vitali
 # -*- coding: iso-8859-15 -*-
-
 import sqlite3 as lite
 
 con = lite.connect('zeustrackerhosts.sqlite')
 f = open('hosts.txt','w+')
 
 with con:    
-    
     cur = con.cursor()    
     cur.execute("SELECT name FROM Hosts")
-
     rows = cur.fetchall()
-
     for row in rows:
         f.write(' '.join(row) + '\n')
  
