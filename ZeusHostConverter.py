a2209f4dafd33f6213409176420a59bffa8136ba
Vitali's Repo <vkremez@users.noreply.github.com>
2016-01-11 06:26:33 -0500

    Update ZeusHostConverter.py

--- ZeusHostConverter.py
+++ ZeusHostConverter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # author = Vitali
-
+# -*- coding: utf-8 -*-
 import urllib
 import json
 serviceurl = "http://ip-api.com/json/"
@@ -9,9 +9,16 @@ w = open('where.data', 'w+')
 count = 0
 for line in f:
     if count > 150 : break
-    url = urllib.urlopen(serviceurl+line)
-    data = url.read()
-    json_data = json.loads(data)
-    w.write(json_data['city']+ '\n')
+    try:
+        url = urllib.urlopen(serviceurl+line)
+        data = url.read()
+        json_data = json.loads(data)
+        w.write(json_data['city']+ '\n')
+    except KeyError, e:
+        print 'Error:', e
+        continue
+    except UnicodeEncodeError, e:
+    	print 'Error:', e
+    	continue
 f.close()
 w.close()
